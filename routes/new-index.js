6cd1b5a5a94bd5be4600283f59a3efd3ae618386
Lucas Moletta <48385698+amiralezer@users.noreply.github.com>
2023-07-10 13:13:13 -0300

    Update new-index.js

--- routes/new-index.js
+++ routes/new-index.js
@@ -3,14 +3,14 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://<username>:<password>@<cluster>-vgz77.azure.mongodb.net/test?retryWrites=true&w=majority";
+var url = "mongodb+srv://admin:admin@healthhub.wgc0tna.mongodb.net/?retryWrites=true&w=majority";
 
 
 router.get('/', (req, res, next) => {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("<database>");
-		dbo.collection("collection1").find({}).toArray(function(err, result) {
+		var dbo = db.db("healthhub");
+		dbo.collection("users").find({}).toArray(function(err, result) {
 			if (err) throw err;
 			console.log('Mongo data coming in hot')
     		console.log(result);
